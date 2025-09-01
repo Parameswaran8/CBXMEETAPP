9a2c52a27a3c806fd85d602d596ed1800ae8dac2
Parameswaran8 <paramsir8@gmail.com>
2025-09-01 16:35:54 +0530

    final logout working

---
+++ android/app/.cxx/RelWithDebInfo/91b643s2/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/Users/harekrishn/.gradle/caches/8.12/transforms/bed008b7c3772c7d5adf4fefb31dbc50/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/harekrishn/.gradle/caches/8.12/transforms/bed008b7c3772c7d5adf4fefb31dbc50/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android/app/.cxx/RelWithDebInfo/91b643s2/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/harekrishn/.gradle/caches/8.12/transforms/8f764e3d611f1b04c96bf1f7360c3f96/transformed/hermes-android-0.78.3-release/prefab/modules/libhermes/libs/android.x86/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/harekrishn/.gradle/caches/8.12/transforms/8f764e3d611f1b04c96bf1f7360c3f96/transformed/hermes-android-0.78.3-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
